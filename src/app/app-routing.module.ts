299977b3cf9c993729962170eb4128eac884e1a5
David <dmoreno788@gmail.com>
2020-11-01 07:37:07 +0100

    Creadas vista Movies y Details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesComponent } from './views/movies/movies.component';
+import { DetailsComponent } from './views/details/details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MoviesComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'details', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './views/movies/movies.component';
+import { DetailsComponent } from './views/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
